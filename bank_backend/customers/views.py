0acfce218fc7e2d8dbd9b03c722363b496cf60ec
matias <o_maty@yahoo.com.ar>
2024-11-15 14:50:03 -0300

    lista de cuentas
    cambio a lista de cuentas para que solo muestre las cuentas pertenecientes al usuario

--- bank_backend/customers/views.py
+++ bank_backend/customers/views.py
@@ -6,16 +6,18 @@ from .forms import UserRegistrationForm
 from django.contrib.auth import login, logout
 from django.shortcuts import render, redirect, get_object_or_404
 
-
 class CustomerListView(LoginRequiredMixin, ListView):
     model = Customer
     template_name = 'customers/customer_list.html'
     context_object_name = 'customers'
 
+    def get_queryset(self):
+        # Filtrar clientes por el usuario que está logueado
+        return Customer.objects.filter(user=self.request.user)
+
 @login_required
 def customer_detail(request):
     customer = get_object_or_404(Customer, user=request.user)
-    print(customer)
     return render(request, 'customers/customer_detail.html', {'customer': customer})
 
 def register(request):
