bc479cad036d9592aba12665499548e2b72049fc
Pato-deve <patriciomartineztrabajo@gmail.com>
2024-11-15 14:07:14 -0300

    pendent fix in customer_detail

--- bank_backend/customers/views.py
+++ bank_backend/customers/views.py
@@ -1,19 +1,22 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from .models import Customer
 from .forms import UserRegistrationForm
 from django.contrib.auth import login, logout
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+
 
 class CustomerListView(LoginRequiredMixin, ListView):
     model = Customer
     template_name = 'customers/customer_list.html'
     context_object_name = 'customers'
 
-class CustomerDetailView(LoginRequiredMixin, DetailView):
-    model = Customer
-    template_name = 'customers/customer_detail.html'
-    context_object_name = 'customer'
+@login_required
+def customer_detail(request):
+    customer = get_object_or_404(Customer, user=request.user)
+    print(customer)
+    return render(request, 'customers/customer_detail.html', {'customer': customer})
 
 def register(request):
     if request.method == 'POST':
