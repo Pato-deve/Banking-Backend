e4a50a9bbb9727942082159b66247aecbd2dbf74
matias <o_maty@yahoo.com.ar>
2024-11-16 01:15:12 -0300

    register
    al registrarse manda al usuario directo a la app sin tener que hacer el login

--- bank_backend/customers/views.py
+++ bank_backend/customers/views.py
@@ -25,8 +25,9 @@ def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('login')
+            user = form.save()
+            login(request, user)  # Loguea automáticamente al usuario
+            return redirect('/')  # Redirige al home o cualquier otra vista principal
     else:
         form = UserRegistrationForm()
     return render(request, 'customers/register.html', {'form': form})
